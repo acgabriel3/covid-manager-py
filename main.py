ae3fa133f595bb619c8c3c65609b28b74c419d67
acgabriel <gabriel_alvesozorio@hotmail.com>
2020-03-20 20:58:08 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#estrutura inicial do manager
+
+#ideia central:
+
+#Capacidade de execucao de scripts em R ou em python no geral, com tempos de execucao determinados
+#Capacidade de execucao de rotinas proprias e intermitentes, em tempo real (ou ao menos em um curto intervalo de tempo)
+
+#-main
+    #-model
+    #-Executor
+    #-Definidor de scritps
+
+
+def if __name__ == "__main__":
+    
+    while True:
+
+        time_events = None
+
+        break
+    
+    pass
