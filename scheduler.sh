ef2c48f8c45aadead40e2bf9313845151395ea13
Matheus Lemos <matheus@ajudici.com.br>
2020-03-22 13:41:48 -0300

    Updated: Correcão da execucão do at e logfiles
    Criacão de entradas no 'at' foram corrigidas;
    Output da execucão dos scripts são logados em arquivos na pasta /tmp
    (substituir futuramente por um parâmetro do script);
    Correcão da execucão de scripts R.

--- scheduler.sh
+++ scheduler.sh
@@ -7,7 +7,19 @@ scriptpattern=".*\.(R$|py$)"
 cronstatus=$(ps -ef | grep crond)
 atdstatus=$(ps -e | grep -e "atd$")
 
+get_executable() {
+	case $1 in
+		"py") executable="python" ;;
+		"R") executable="R -f" ;;
+	esac
+	echo "$executable"	
+}
+
 # Gera o schedule do crontab e edita o arquivo
+# Parâmetros:
+#	$1 - Path do arquivo
+#	$2 - Valor numérico com o período de repeticão
+#	$3 - Extensão do arquivo
 gen_crontab() {
 	flag=${2:(-1)}
 	value=${2:0:(-1)}
@@ -18,29 +30,35 @@ gen_crontab() {
 		"H") intervals=$(echo "$intervals" | sed -e "s/\*/\*\/${value}/2") ;; 
 	esac
 
-	case $extension in
-		"py") executable="python" ;;
-		"R") executable="R" ;;
-	esac	
-	cronjob=$(printf "%b %b %b\n" "$intervals" "$executable" "$1")
+	executable=$(get_executable "$3")
+	cronjob=$(printf "%b %b %b >> /tmp/logfile\n" "$intervals" "$executable" "$1")
 
 	(crontab -l 2>/dev/null; echo "$cronjob") | crontab -
 }
 
 # Cria uma entrada no 'at'
+# Parâmetros:
+# 	$1 - Path do arquivo
+# 	$2 - Hora da execucão do script
+#	$3 - Extensão do arquivo
 gen_at() {
 	time=$(echo $2 | sed -r 's/.{2}/&:/')
-	echo "$1" | at "$time" 2> /dev/null
+	executable=$(get_executable $3)
+	command=$(echo "$executable $1 >> /tmp/logfile2")
+	echo "$command" | at "$time" 2> /dev/null
 }
 
 # Verifica se o script deve ser executado periodicamente (R) ou uma vez (E)
+# Parâmetros:
+#	$1 - Path do arquivo
+#	$2 - Flags extraídas do arquivo
 generate_schedule() {
 	flag=${2:0:1}	
 	value=${2:1}
 	extension="${file##*.}"
 
 	case $flag in
-		"E") gen_at $1 "$value" ;;
+		"E") gen_at $1 "$value" "$extension" ;;
 		"R") gen_crontab $1 "$value" "$extension" 
 	esac
 }
@@ -61,7 +79,7 @@ display_help() {
 	echo " -h  Exibe esse menu de ajuda"
 }
 
-
+# Main
 if [[ -z $1 || $* == *-h* ]]
 then
 	display_help
