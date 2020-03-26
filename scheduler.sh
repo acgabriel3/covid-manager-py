d3938bf753873ea71b9fdb07ec718eaff80c713e
Matheus Lemos <matheuslemosf@protonmail.com>
2020-03-26 20:25:54 -0300

    Refactored: Mudancas para padrão POSIX
    Código modificado para remover comandos e estruturas nativos do bash,
    para ficar mais parecido com o padrão de shell POSIX.

--- scheduler.sh
+++ scheduler.sh
@@ -5,8 +5,8 @@
 scriptfolder=$1
 logfolder=$2
 scriptpattern=".*\.(R$|py$)"
-cronstatus=$(ps -ef | grep crond)
-atdstatus=$(ps -e | grep -e "atd$")
+cronstatus=$(pgrep crond)
+atdstatus=$(pgrep "atd$")
 
 get_executable() {
 	case $1 in
@@ -42,9 +42,9 @@ gen_crontab() {
 # 	$2 - Hora da execucão do script
 #	$3 - Extensão do arquivo
 gen_at() {
-	time=$(echo $2 | sed -r 's/.{2}/&:/')
+	time=$(echo "$2" | sed -r 's/.{2}/&:/')
 	executable=$(get_executable "$3")
-	command=$(echo "$timestamp" "$executable $1 >> ${logfolder}/atlogs")
+	command=$(echo "$executable '$1' >> ${logfolder}/atlogs")
 	echo "$command" | at "$time" 2> /dev/null
 }
 
@@ -58,15 +58,15 @@ generate_schedule() {
 	extension="${file##*.}"
 
 	case $flag in
-		"E") gen_at $1 "$value" "$extension" ;;
-		"R") gen_crontab $1 "$value" "$extension" 
+		"E") gen_at "$1" "$value" "$extension" ;;
+		"R") gen_crontab "$1" "$value" "$extension" 
 	esac
 }
 
 # Faz o parse dos arquivos e extrai as flags
 parse_schedule() {
 	for file in $1; do
-		flags=$(sed -e "s/.*@//" -e "s/\.[^.]*$//"  <<< $file)
+		flags=$(echo "$file" | sed -e "s/.*@//" -e "s/\.[^.]*$//")
 		generate_schedule "$file" "$flags"
 	done
 }
@@ -80,19 +80,19 @@ display_help() {
 }
 
 # Main
-if [[ -z $1 || -z $2 ||$* == *-h* ]]
+if test -z "$1" || test -z "$2" || test "$*" == "*-h*"
 then
 	display_help
 	exit  1
 fi
 
-if [[ -z "$cronstatus" || -z $atdstatus ]]
+if test -z "$cronstatus" || test -z "$atdstatus"
 then
 	echo "cron ou atd não iniciados"
 	exit 1
 else
 	logfolder=$(echo "$logfolder" | sed 's/\/$//')
-	files=$(find $scriptfolder -type f -regextype egrep -regex "$scriptpattern" \
+	files=$(find "$scriptfolder" -type f -regextype egrep -regex "$scriptpattern" \
 	-exec realpath {} \;)
 	parse_schedule "$files"
 	exit 0
